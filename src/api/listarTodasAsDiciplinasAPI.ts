f450e641a8e9880a4dd94436f665a20d1881ada3
Samuel Luz Santana <slsamuelluz@gmail.com>
2024-03-07 12:44:21 -0300

    lista de todas as diciplinas no json

--- src/api/listarTodasAsDiciplinasAPI.ts
+++ src/api/listarTodasAsDiciplinasAPI.ts
@@ -1,13 +1,41 @@
-import axios from "axios";
+"use server";
+
 import puppeteer from "puppeteer";
 
 const URL_API_MATRICULA =
   "https://matricula.ufabc.edu.br/cache/todasDisciplinas.js";
 
 async function listaTodasDisciplinasAPI() {
-  
-    console.log(URL_API_MATRICULA);
-    
+  try {
+    const browser = await puppeteer.launch({});
+    const page = await browser.newPage();
+    await page.goto(URL_API_MATRICULA);
+
+    const data = await page.evaluate(() => {
+      const preElement = document.querySelector("pre");
+
+      if (preElement && preElement.textContent !== null) {
+        const preContent = preElement.textContent.trim();
+        const conteudoMatch = preContent.match(/todasDisciplinas=(\[.*\]);/);
+
+        if (conteudoMatch && conteudoMatch[1]) {
+          return conteudoMatch[1];
+        }
+      }
+
+      return null;
+    });
+
+    if (data !== null) {
+      console.log(data);
+      await browser.close();
+      return JSON.parse(data);
+    } else {
+      throw new Error("Failed to extract data from the page.");
+    }
+  } catch (error) {
+    throw error;
+  }
 }
 
 export { listaTodasDisciplinasAPI };
--- src/components/GridMaterias.tsx
+++ src/components/GridMaterias.tsx
@@ -1,18 +1,26 @@
+import { useEffect, useState } from "react";
 import { listaTodasDisciplinasAPI } from "@/api/listarTodasAsDiciplinasAPI";
 
 export default function GridMaterias() {
+  const [isLoading, setIsLoading] = useState(true);
+
   async function listaTodasDisciplinas() {
+    setIsLoading(true);
+
     try {
       const response = await listaTodasDisciplinasAPI();
       console.log(response);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
-  return (
-    <>
-      <p onClick={() => listaTodasDisciplinas()}>Grid</p>
-    </>
-  );
+  useEffect(() => {
+   
+    listaTodasDisciplinas();
+  }, []); 
+
+  return <>{!isLoading && <p>grid</p>}</>;
 }
