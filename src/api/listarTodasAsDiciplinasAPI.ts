316337037e475a93a47637fbd015d35ed1393fe7
Samuel Luz Santana <samuelluzsantana@outlook.com>
2024-03-31 13:14:02 -0300

    colocando um progress bar na listagem das disciplinas

--- src/api/listarTodasAsDiciplinasAPI.ts
+++ src/api/listarTodasAsDiciplinasAPI.ts
@@ -1,13 +1,10 @@
 "use server";
 
-import axios from "axios";
-
-const URL_API_MATRICULA =
-  "https://matricula.ufabc.edu.br/cache/todasDisciplinas.js";
+const URL_API_MATRICULA = "https://matricula.ufabc.edu.br/cache/todasDisciplinas.js";
 
 async function listaTodasDisciplinasAPI(): Promise<Discipline[]> {
   try {
-    const { data } = await axios.get(URL_API_MATRICULA, {
+    const response = await fetch(URL_API_MATRICULA, {
       headers: {
         Accept: `text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9`,
         Host: `matricula.ufabc.edu.br`,
@@ -15,9 +12,19 @@ async function listaTodasDisciplinasAPI(): Promise<Discipline[]> {
       },
     });
 
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.text();
+
     const conteudoMatch = data.match(/todasDisciplinas=(\[.*\]);/);
-    const formatedData = conteudoMatch[1];
+    console.log(conteudoMatch);
+
+    if (conteudoMatch === null) { throw new Error('Failed to match data');}
+    const formatedData = conteudoMatch[1] ;
     const disciplines: Discipline[] = JSON.parse(formatedData);
+
 
     // Map disciplines to the required format
     const formattedDisciplines = disciplines.map(discipline => {
@@ -36,13 +43,13 @@ async function listaTodasDisciplinasAPI(): Promise<Discipline[]> {
           horarios: discipline.horarios,
           vagas: discipline.vagas,
         };
-      }  else {
-        return null; 
+      } else {
+        return null;
       }
     });
 
     const filteredDisciplines = formattedDisciplines.filter(discipline => discipline !== null) as Discipline[];
-    
+
     return filteredDisciplines;
   } catch (error) {
     throw error;
--- src/components/GridMaterias.tsx
+++ src/components/GridMaterias.tsx
@@ -2,14 +2,13 @@
 
 import { useEffect, useState } from "react";
 import { listaTodasDisciplinasAPI } from "@/api/listarTodasAsDiciplinasAPI";
-import axios from "axios";
 import {
   Button,
-  Chip,
   Dropdown,
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
+  Progress,
   ScrollShadow,
   Spinner,
   Table,
@@ -22,11 +21,14 @@ import {
 import { getDayName } from "@/services/dayformats";
 
 export default function GridMaterias() {
+
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [courses, setCourses] = useState<Discipline[]>([]);
+  const revalidateTime = 1300;
 
   const isSmallScreen = window.innerWidth < 450;
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const visibleColumns = [
     { id: "sigla", value: "Sigla" },
     { id: "nome", value: "Nome" },
@@ -50,7 +52,7 @@ export default function GridMaterias() {
     setSelectedColumns(selectedKeys);
   };
 
-  function verificaSelecionado(valor: string): boolean {
+  function verifySelecteds(valor: string): boolean {
     return !selectedKeys.has(valor);
   }
 
@@ -58,21 +60,18 @@ export default function GridMaterias() {
     setIsLoading(true);
     try {
       const response = await listaTodasDisciplinasAPI();
-
-      if (response && response.length > 0) {
-        const formattedCourses = response
-          .map((course: Discipline) => ({
-            nome: course.nome,
-            sigla: course.sigla,
-            nome_campus: course.nome_campus, // Include nome_campus directly
-            codigo: course.codigo,
-            horarios: course.horarios,
-            vagas: course.vagas,
-            periodo: course.periodo,
-            turma: course.turma,
-          }));
-        setCourses((prevCourses) => [...prevCourses, ...formattedCourses]);
-      }
+      const formattedCourses = response
+        .map((course: Discipline) => ({
+          nome: course.nome,
+          sigla: course.sigla,
+          nome_campus: course.nome_campus, // Include nome_campus directly
+          codigo: course.codigo,
+          horarios: course.horarios,
+          vagas: course.vagas,
+          periodo: course.periodo,
+          turma: course.turma,
+        }));
+      setCourses((prevCourses) => [...prevCourses, ...formattedCourses]);
     } catch (error) {
       console.log(error);
     } finally {
@@ -95,111 +94,131 @@ export default function GridMaterias() {
 
   useEffect(() => {
     listaTodasDisciplinas();
+    const interval = setInterval(listaTodasDisciplinas, revalidateTime * 1000); // Revalidate every hour
+    return () => clearInterval(interval);
   }, []);
+
 
   return (
     <>
-      <ScrollShadow visibility={"bottom"} className="h-[30em]">
-        <Table
-          isStriped
-          isHeaderSticky
-          removeWrapper
-          aria-label="Lista de Disciplinas"
-          className="table-disciplinas"
-          topContent={
-            <>
-              <Dropdown>
-                <DropdownTrigger>
-                  <Button size="sm" variant="flat">
-                    Columns
-                  </Button>
-                </DropdownTrigger>
-                <DropdownMenu
-                  disallowEmptySelection
-                  aria-label="Table Columns"
-                  closeOnSelect={false}
-                  selectedKeys={selectedColumns}
-                  selectionMode="multiple"
-                  onSelectionChange={handleSelectionChange}
+      {
+        isLoading ?
+          <Progress
+            size="sm"
+            isIndeterminate
+            classNames={{
+              indicator: "bg-[#00007c]",
+            }}
+          />
+
+          :
+          <ScrollShadow visibility={"bottom"} className="h-[30em]">
+            <Table
+              isStriped
+              isHeaderSticky
+              removeWrapper
+              aria-label="Lista de Disciplinas"
+              className="table-disciplinas"
+              topContent={
+                <>
+                  <Dropdown>
+                    <DropdownTrigger>
+                      <Button size="sm" variant="flat">
+                        Columns
+                      </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu
+                      disallowEmptySelection
+                      aria-label="Table Columns"
+                      closeOnSelect={false}
+                      selectedKeys={selectedColumns}
+                      selectionMode="multiple"
+                      onSelectionChange={handleSelectionChange}
+                    >
+                      {visibleColumns.map((column) => (
+                        <DropdownItem key={column.id}>{column.value}</DropdownItem>
+                      ))}
+                    </DropdownMenu>
+                  </Dropdown>
+                </>
+              }
+            >
+              <TableHeader className="px-5">
+                <TableColumn hidden={verifySelecteds("sigla")} key="sigla">
+                  Sigla
+                </TableColumn>
+                <TableColumn hidden={verifySelecteds("nome")} key="nome">
+                  Nome
+                </TableColumn>
+                <TableColumn hidden={verifySelecteds("turma")} key="turma">
+                  Turma
+                </TableColumn>
+                <TableColumn hidden={verifySelecteds("periodo")} key="periodo">
+                  Periodo
+                </TableColumn>
+                <TableColumn
+                  hidden={verifySelecteds("nome_campus")}
+                  key="nome_campus"
                 >
-                  {visibleColumns.map((column) => (
-                    <DropdownItem key={column.id}>{column.value}</DropdownItem>
-                  ))}
-                </DropdownMenu>
-              </Dropdown>
-            </>
-          }
-        >
-          <TableHeader className="px-5">
-            <TableColumn hidden={verificaSelecionado("sigla")} key="sigla">
-              Sigla
-            </TableColumn>
-            <TableColumn hidden={verificaSelecionado("nome")} key="nome">
-              Nome
-            </TableColumn>
-            <TableColumn hidden={verificaSelecionado("turma")} key="turma">
-              Turma
-            </TableColumn>
-            <TableColumn hidden={verificaSelecionado("periodo")} key="periodo">
-              Periodo
-            </TableColumn>
-            <TableColumn
-              hidden={verificaSelecionado("nome_campus")}
-              key="nome_campus"
-            >
-              Campus
-            </TableColumn>
-            <TableColumn hidden={verificaSelecionado("codigo")} key="codigo">
-              Código
-            </TableColumn>
-            <TableColumn
-              hidden={verificaSelecionado("horarios")}
-              key="horarios"
-            >
-              Horários
-            </TableColumn>
-            <TableColumn hidden={verificaSelecionado("vagas")} key="vagas">
-              Vagas
-            </TableColumn>
-          </TableHeader>
-          <TableBody emptyContent={"Disciplinas ainda não carregadas."}>
-            {courses.map((course, index) => (
-              <TableRow key={index}>
-                <TableCell hidden={verificaSelecionado("sigla")}>
-                  {course.sigla}
-                </TableCell>
-                <TableCell hidden={verificaSelecionado("nome")}>
-                  {course.nome}
-                </TableCell>
-                <TableCell hidden={verificaSelecionado("turma")}>
-                  {course.turma}
-                </TableCell>
-                <TableCell hidden={verificaSelecionado("periodo")}>
-                  {course.periodo}
-                </TableCell>
-                <TableCell hidden={verificaSelecionado("nome_campus")}>
-                  {course.nome_campus}
-                </TableCell>
-                <TableCell hidden={verificaSelecionado("codigo")}>
-                  {course.codigo}
-                </TableCell>
-                <TableCell hidden={verificaSelecionado("horarios")}>
-                  <ul>
-                    {course.horarios.map((horario, idx) => (
-                      <li key={idx}>
-                        {getDayName(horario.semana)}: {horario.horas.join(", ")}
-                      </li>
-                    ))}
-                  </ul>
-                </TableCell>
-                <TableCell hidden={verificaSelecionado("vagas")}>
-                  {course.vagas}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </ScrollShadow>
+                  Campus
+                </TableColumn>
+                <TableColumn hidden={verifySelecteds("codigo")} key="codigo">
+                  Código
+                </TableColumn>
+                <TableColumn
+                  hidden={verifySelecteds("horarios")}
+                  key="horarios"
+                >
+                  Horários
+                </TableColumn>
+                <TableColumn hidden={verifySelecteds("vagas")} key="vagas">
+                  Vagas
+                </TableColumn>
+              </TableHeader>
+              <TableBody
+                emptyContent={!isLoading ? "Disciplinas não carregadas." : ''}
+                isLoading={isLoading}
+                loadingContent={<Spinner label="Loading..." />}
+              >
+                {courses.map((course, index) => (
+                  <TableRow key={index}>
+                    <TableCell hidden={verifySelecteds("sigla")}>
+                      {course.sigla}
+                    </TableCell>
+                    <TableCell hidden={verifySelecteds("nome")}>
+                      {course.nome}
+                    </TableCell>
+                    <TableCell hidden={verifySelecteds("turma")}>
+                      {course.turma}
+                    </TableCell>
+                    <TableCell hidden={verifySelecteds("periodo")}>
+                      {course.periodo}
+                    </TableCell>
+                    <TableCell hidden={verifySelecteds("nome_campus")}>
+                      {course.nome_campus}
+                    </TableCell>
+                    <TableCell hidden={verifySelecteds("codigo")}>
+                      {course.codigo}
+                    </TableCell>
+                    <TableCell hidden={verifySelecteds("horarios")}>
+                      <ul>
+                        {course.horarios.map((horario, idx) => (
+                          <li key={idx}>
+                            {getDayName(horario.semana)}: {horario.horas.join(", ")}
+                          </li>
+                        ))}
+                      </ul>
+                    </TableCell>
+                    <TableCell hidden={verifySelecteds("vagas")}>
+                      {course.vagas}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </ScrollShadow>
+      }
+
     </>
   );
 }
