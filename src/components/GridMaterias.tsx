da81ad999323f89b1c4e7ea3d4bdafbccee05313
Samuel Luz Santana <samuelluzsantana@outlook.com>
2024-06-30 17:00:44 -0300

    tr e td

--- src/components/GridMaterias.tsx
+++ src/components/GridMaterias.tsx
@@ -1,17 +1,22 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { useEffect, useState } from "react";
 import { listaTodasDisciplinasAPI } from "@/api/listarTodasAsDiciplinasAPI";
-import { Progress } from "@nextui-org/react"
-import { getDayName } from "@/services/dayformats";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Progress,
+  Spinner,
+  ScrollShadow,
+} from "@nextui-org/react";
 
 export default function GridMaterias() {
-
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [disciplines, setDisciplines] = useState<Discipline[]>([]);
-
-  const disciplinesFromLocalStorage = localStorage.getItem('disciplines')
+  const disciplinesFromLocalStorage = localStorage.getItem("disciplines");
 
   const revalidateTime = 1300;
 
@@ -33,30 +38,48 @@ export default function GridMaterias() {
       : visibleColumns.map((column) => column.id)
   );
 
+  const [selectedRows, setSelectedRows] = useState<string[]>([]);
+
+  const selectedKeys = new Set(selectedColumns);
+
+  const handleSelectionChange = (selectedKeys: any) => {
+    setSelectedColumns(selectedKeys);
+  };
+
+  const handleRowSelectionChange = (selectedRows: any) => {
+    setSelectedRows(selectedRows);
+    console.log(selectedRows);
+  };
+
+  function verifySelecteds(valor: string): boolean {
+    return !selectedKeys.has(valor);
+  }
 
   async function listaTodasDisciplinas() {
     if (disciplinesFromLocalStorage) {
       setDisciplines(JSON.parse(disciplinesFromLocalStorage));
       setIsLoading(false);
-      return
+      return;
     }
 
     setIsLoading(true);
 
     try {
       const response = await listaTodasDisciplinasAPI();
-      const formattedDisciplines = response
-        .map((course: Discipline) => ({
-          nome: course.nome,
-          sigla: course.sigla,
-          nome_campus: course.nome_campus, // Include nome_campus directly
-          codigo: course.codigo,
-          horarios: course.horarios,
-          vagas: course.vagas,
-          periodo: course.periodo,
-          turma: course.turma,
-        }));
-      setDisciplines((prevCourses) => [...prevCourses, ...formattedDisciplines]);
+      const formattedDisciplines = response.map((course: Discipline) => ({
+        nome: course.nome,
+        sigla: course.sigla,
+        nome_campus: course.nome_campus, // Include nome_campus directly
+        codigo: course.codigo,
+        horarios: course.horarios,
+        vagas: course.vagas,
+        periodo: course.periodo,
+        turma: course.turma,
+      }));
+      setDisciplines((prevCourses) => [
+        ...prevCourses,
+        ...formattedDisciplines,
+      ]);
     } catch (error) {
       console.log(error);
     } finally {
@@ -90,22 +113,147 @@ export default function GridMaterias() {
 
   return (
     <>
-      {
-        isLoading ?
-          <Progress
-            aria-label="Loading progress"
-            size="sm"
-            isIndeterminate
-            classNames={{
-              indicator: "bg-[#00007c]",
-            }}
-          />
-          :
-          <>
-            <p>Tabela</p>
-          </>
-      }
-
+      {isLoading ? (
+        <Progress
+          size="sm"
+          isIndeterminate
+          classNames={{
+            indicator: "bg-[#00007c]",
+          }}
+        />
+      ) : (
+        <ScrollShadow visibility={"bottom"} className="h-[30em]">
+          <div className="overflow-x-auto">
+            <Dropdown>
+              <DropdownTrigger>
+                <Button size="sm" variant="flat">
+                  Colunas
+                </Button>
+              </DropdownTrigger>
+              <DropdownMenu
+                disallowEmptySelection
+                aria-label="Table Columns"
+                closeOnSelect={false}
+                selectedKeys={selectedColumns}
+                selectionMode="multiple"
+                onSelectionChange={handleSelectionChange}
+              >
+                {visibleColumns.map((column) => (
+                  <DropdownItem key={column.id}>{column.value}</DropdownItem>
+                ))}
+              </DropdownMenu>
+            </Dropdown>
+            <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+              <thead className="">
+                <tr>
+                  <th
+                    className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300 ${
+                      verifySelecteds("sigla") ? "hidden" : ""
+                    }`}
+                  >
+                    Sigla
+                  </th>
+                  <th
+                    className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300 ${
+                      verifySelecteds("nome") ? "hidden" : ""
+                    }`}
+                  >
+                    Nome
+                  </th>
+                  <th
+                    className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300 ${
+                      verifySelecteds("turma") ? "hidden" : ""
+                    }`}
+                  >
+                    Turma
+                  </th>
+                  <th
+                    className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300 ${
+                      verifySelecteds("periodo") ? "hidden" : ""
+                    }`}
+                  >
+                    Periodo
+                  </th>
+                  <th
+                    className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300 ${
+                      verifySelecteds("nome_campus") ? "hidden" : ""
+                    }`}
+                  >
+                    Campus
+                  </th>
+                  <th
+                    className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300 ${
+                      verifySelecteds("codigo") ? "hidden" : ""
+                    }`}
+                  >
+                    Código
+                  </th>
+                  <th
+                    className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300 ${
+                      verifySelecteds("vagas") ? "hidden" : ""
+                    }`}
+                  >
+                    Vagas
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white dark:bg-gray-900 divide-y divide-gray-200 dark:divide-gray-700">
+                {disciplines.map((course, index) => (
+                  <tr
+                    key={index}
+                    className={`cursor-pointer ${
+                      selectedRows.includes(course.sigla)
+                        ? "bg-gray-100 dark:bg-gray-700"
+                        : ""
+                    }`}
+                    onClick={() => handleRowSelectionChange([course.sigla])}
+                  >
+                    <td
+                      className={`${verifySelecteds("sigla") ? "hidden" : ""}`}
+                    >
+                      {course.sigla}
+                    </td>
+                    <td
+                      className={`${verifySelecteds("nome") ? "hidden" : ""}`}
+                    >
+                      {course.nome}
+                    </td>
+                    <td
+                      className={`${verifySelecteds("turma") ? "hidden" : ""}`}
+                    >
+                      {course.turma}
+                    </td>
+                    <td
+                      className={`${
+                        verifySelecteds("periodo") ? "hidden" : ""
+                      }`}
+                    >
+                      {course.periodo}
+                    </td>
+                    <td
+                      className={`${
+                        verifySelecteds("nome_campus") ? "hidden" : ""
+                      }`}
+                    >
+                      {course.nome_campus}
+                    </td>
+                    <td
+                      className={`${verifySelecteds("codigo") ? "hidden" : ""}`}
+                    >
+                      {course.codigo}
+                    </td>
+                    <td
+                      className={`${verifySelecteds("vagas") ? "hidden" : ""}`}
+                    >
+                      {course.vagas}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </ScrollShadow>
+      )}
     </>
   );
 }
